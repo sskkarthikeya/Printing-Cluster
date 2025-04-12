261371567643a4bd13ab704bf54de57e2330495f
sskkarthikeya <156413339+sskkarthikeya@users.noreply.github.com>
2025-04-12 15:31:26 +0530

    Update logout.php
    added session functions

--- auth/logout.php
+++ auth/logout.php
@@ -1,5 +1,9 @@
 <?php
+session_start();
+session_unset();        // Optional: Unsets all session variables
+session_destroy();      // Destroys the session
+
+// Redirect to home page
 header("Location: ../Home/index.php");
 exit();
-
 ?>
